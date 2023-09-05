b0779a058a12bf0ca8415a55a8ddb7621fd84a8e
Jacob Humston <jhumston24@sciototech.org>
2023-09-05 14:12:44 -0400

    .

--- scripts/nodejs/repo-basic.sh
+++ scripts/nodejs/repo-basic.sh
@@ -1,29 +1,35 @@
 #!/bin/sh
 
+# Prevent command echos.
 @echo off
 
+# Clear console.
 clear
 
+# Create varaibles.
 name=""
 repoUrl=""
 
+# Ask for folder name.
 echo "> What would you like to name the folder?"
 read name
 clear
 
+# Ask for repo git URL.
 echo "> What is the repo git URL?"
 read repoUrl
 clear
 
-echo "Running... Please wait."
-
+# Create directory, then open it.
 mkdir ./${name}
 cd ./${name}
+
+# Clone the repo and then clean install deps.
 git clone ${repoUrl} .
 npm ci
 
+# Clear ouput and ask closing question.
 clear
-
 echo "> Press ENTER to close..."
 read
 clear
