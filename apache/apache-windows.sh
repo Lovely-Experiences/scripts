db2aff8ca651a2b2043295693d4b91935df92752
Jacob Humston <humstonjacob@gmail.com>
2023-09-14 19:08:09 -0400

    .

--- apache/apache-windows.sh
+++ apache/apache-windows.sh
@@ -5,5 +5,5 @@ mkdir "C:/Server"
 mkdir "C:/Server/Apache"
 mkdir "C:/Server/Content"
 cd "C:/Server"
-curl --insecure -O "https://raw.githubusercontent.com/Lovely-Experiences/scripts/main/apache/apache-windows.zip"
+curl --insecure -O https://raw.githubusercontent.com/Lovely-Experiences/scripts/main/apache/apache-windows.zip
 unzip "./apache-windows.zip" -d "./Apache"
