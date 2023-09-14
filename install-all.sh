c32f4c33cdff4f8fb981286c7a5abc4c3bef3d87
Jacob Humston <humstonjacob@gmail.com>
2023-09-14 18:15:14 -0400

    fixed file name

--- install-all.sh
+++ install-all.sh
@@ -4,7 +4,7 @@
 declare -a files
 files[0]="nodejs/node-repo-basic.sh"
 files[1]="random/console-rainbow.sh"
-files[2]="apache/windows/install.sh"
+files[2]="apache/apache-windows.sh"
 
 path="./scripts-$(date +%s)"
 url="https://raw.githubusercontent.com/Lovely-Experiences/scripts/main/"
