3aade6f4f16e65c7b0136387c07c57c149cce7d9
Jacob Humston <humstonjacob@gmail.com>
2023-09-14 19:09:30 -0400

    .

--- apache/apache-windows.sh
+++ apache/apache-windows.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
 clear
+
 mkdir "C:/Server"
+
 mkdir "C:/Server/Apache"
+
 mkdir "C:/Server/Content"
+
 cd "C:/Server"
+
 curl -k -O "https://raw.githubusercontent.com/Lovely-Experiences/scripts/main/apache/apache-windows.zip"
+
 unzip "./apache-windows.zip" -d "./Apache"
