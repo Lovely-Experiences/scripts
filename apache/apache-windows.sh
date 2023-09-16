eb2885ecddf3533f902caff57e48a0b4f5e8d520
Jacob Humston <humstonjacob@gmail.com>
2023-09-16 15:30:23 -0400

    .

--- apache/apache-windows.sh
+++ apache/apache-windows.sh
@@ -15,4 +15,4 @@ unzip "./apache-windows.zip" -d "./Apache"
 clear
 echo "Finished installation, press any key to open the folder for your web project(s)."
 read
-start %SystemRoot%\explorer.exe "C:/Server/Content"
+explorer "C:/Server/Content"
