9a145d8091a7d5bb927e6b8277566724a73ec4b8
Jacob Humston <humstonjacob@gmail.com>
2023-09-16 15:24:31 -0400

    .

--- apache/apache-windows.sh
+++ apache/apache-windows.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [ -d "C:/Server" ];
+then
+	echo "Server folder is already present, uninstalling."
+    rm -rf C:/Server
+fi
 clear
 mkdir "C:/Server"
 mkdir "C:/Server/Apache"
@@ -7,3 +12,7 @@ mkdir "C:/Server/Content"
 cd "C:/Server"
 curl -O "https://raw.githubusercontent.com/Lovely-Experiences/scripts/main/apache/apache-windows.zip"
 unzip "./apache-windows.zip" -d "./Apache"
+clear
+echo "Finished installation, press any key to open the folder for your web project(s)."
+read
+%SystemRoot%\explorer.exe "C:/Server/Content"
